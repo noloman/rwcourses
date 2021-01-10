9537315a430d69d0a50bb0f09211f77025736c1b
Manuel Lorenzo <manulorenzop@gmail.com>
2021-01-10 17:31:55 +0100

    create CoursesPage

---
+++ lib/ui/courses/courses_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CoursesPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CoursesStatePage();
+}
+
+class _CoursesStatePage extends State<CoursesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('RW courses');
+  }
+}
