babc1ed62f4c3570ebdebafc5ddaf17b44243daf
Manuel Lorenzo <manulorenzop@gmail.com>
2021-01-14 22:02:16 +0100

    inject CoursesRepository to the CoursesController

--- lib/ui/courses/courses_controller.dart
+++ lib/ui/courses/courses_controller.dart
@@ -1,9 +1,10 @@
 import 'package:rw_courses/model/course.dart';
-import 'package:rw_courses/repository/_repository.dart';
 import 'package:rw_courses/repository/repository.dart';
 
 class CoursesController {
-  final Repository _repository = CourseRepository();
+  final Repository _repository;
+
+  CoursesController(this._repository);
 
   Future<List<Course>> fetchCourses(int domainFilter) =>
       _repository.getCourses(domainFilter);
--- lib/ui/courses/courses_page.dart
+++ lib/ui/courses/courses_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rw_courses/constants.dart';
 import 'package:rw_courses/model/course.dart';
+import 'package:rw_courses/repository/_repository.dart';
 import 'package:rw_courses/ui/courses/courses_controller.dart';
 
 class CoursesPage extends StatefulWidget {
@@ -9,7 +10,7 @@ class CoursesPage extends StatefulWidget {
 }
 
 class _CoursesStatePage extends State<CoursesPage> {
-  final _controller = CoursesController();
+  final _controller = CoursesController(CourseRepository());
 
   @override
   Widget build(BuildContext context) => FutureBuilder<List<Course>>(
